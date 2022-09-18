5b4bd19ac6e80e445c50c3b5f57e9a4112a18c46
Jun Jie Ng <ngjunjie13@gmail.com>
2022-09-18 23:54:06 +1000

    update

--- js/vega_lite_vis.js
+++ js/vega_lite_vis.js
@@ -1,4 +1,4 @@
-var vg_1 = "viz-1-map.json";
+var vg_1 = "js/viz_1_map.json";
 vegaEmbed("#viz-1-map", vg_1).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
