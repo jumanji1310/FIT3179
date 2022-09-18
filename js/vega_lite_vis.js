aa820d87ca6a7dde50b7ccb6f05c61a926fdc382
Jun Jie Ng <ngjunjie13@gmail.com>
2022-09-18 13:23:14 +1000

    1:23pm

---
+++ js/vega_lite_vis.js
@@ -0,0 +1,4 @@
+var vg_1 = "viz-1-map.json";
+vegaEmbed("#viz-1-map", vg_1).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
