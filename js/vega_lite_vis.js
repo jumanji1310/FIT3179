567b35ab6997841da66aff2711cb73b9aa58c81a
Jun Jie Ng <ngjunjie13@gmail.com>
2022-09-24 17:27:51 +1000

    Added 2 more viz

--- js/vega_lite_vis.js
+++ js/vega_lite_vis.js
@@ -1,4 +1,16 @@
 var vg_1 = "js/viz_1_map.json";
-vegaEmbed("#viz-1-map", vg_1).then(function(result) {
+var vg_2 = "js/viz_2_scatter.json";
+var vg_3 = "js/viz_3_lollipop.json";
+var vg_4 = "js/viz_4_stacked_bar.json";
+vegaEmbed("#viz-1-map", vg_1, {"actions": false}).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+vegaEmbed("#viz-2-scatter", vg_2, {"actions": false}).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+vegaEmbed("#viz-3-lollipop", vg_3, {"actions": false}).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+vegaEmbed("#viz-4-stacked-bar", vg_4, {"actions": false}).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
