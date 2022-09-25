9377ee06c585b34849553d0f0e4bad0e62f582c5
Jun Jie Ng <ngjunjie13@gmail.com>
2022-09-25 22:09:09 +1000

    Updated lollipop chart

--- js/vega_lite_vis.js
+++ js/vega_lite_vis.js
@@ -3,6 +3,7 @@ var vg_2 = "js/viz_2_scatter.json";
 var vg_3 = "js/viz_3_lollipop.json";
 var vg_4 = "js/viz_4_stacked_bar.json";
 var vg_5 = "js/viz_5_line.json"
+var vg_6 = "js/viz_6_donut.json"
 vegaEmbed("#viz-1-map", vg_1, {"actions": false}).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
@@ -16,5 +17,8 @@ vegaEmbed("#viz-4-stacked-bar", vg_4, {"actions": false}).then(function(result) 
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 vegaEmbed("#viz-5-line", vg_5, {"actions": false}).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+vegaEmbed("#viz-6-donut", vg_6, {"actions": false}).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
